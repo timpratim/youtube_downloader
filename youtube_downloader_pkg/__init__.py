dff70c4617e9edd2dabdfee6b97371fe605f73bb
timpratim <pratimbhosale@gmail.com>
2025-01-23 20:24:49 +0100

    Upgrade package

--- youtube_downloader_pkg/__init__.py
+++ youtube_downloader_pkg/__init__.py
@@ -1,6 +1,6 @@
 """YouTube Video Downloader Package"""
 from youtube_downloader_pkg.__main__ import main, VideoDownloader
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ['main', 'VideoDownloader']
